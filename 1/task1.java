b0f96b5312c8be65eec87e0e818c0c1c52f0fd1e
am_6733 <anton6733@gmail.com>
2022-10-05 22:21:57 +0300

    1st java bullet, sort of

---
+++ 1/task1.java
@@ -0,0 +1,9 @@
+class task1{
+    public static void todo(){
+        System.out.println("Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.");
+    }
+    public static void main(String[] args){
+        todo();
+
+    }
+}
